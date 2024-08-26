d9356b7f969732106137aca419d11c9b9684fbe8
Najlae Abarghache <najlae.abarghache1@gmail.com>
2024-08-26 19:36:23 +0100

    deploying to vercel

--- index.js
+++ index.js
@@ -27,7 +27,16 @@ app.use(
   })
 )
 
-app.options('*', cors())
+app.options('*', (req, res) => {
+  res.header(
+    'Access-Control-Allow-Origin',
+    'https://media-frontend-five.vercel.app'
+  )
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+  res.header('Access-Control-Allow-Credentials', 'true')
+  res.sendStatus(200)
+})
 
 dotenv.config()
 
