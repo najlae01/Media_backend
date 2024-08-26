1f114540ccecde19cd0765b26d9cb4e2045d9bb1
Najlae Abarghache <najlae.abarghache1@gmail.com>
2024-08-26 09:45:29 +0100

    updates

--- index.js
+++ index.js
@@ -19,7 +19,10 @@ app.use('/images', express.static('images'))
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(
-  cors({ origin: 'https://media-frontend-ymug.vercel.app', credentials: true })
+  cors({
+    origin: 'https://media-frontend-ymug.vercel.app',
+    credentials: true,
+  })
 )
 
 dotenv.config()
